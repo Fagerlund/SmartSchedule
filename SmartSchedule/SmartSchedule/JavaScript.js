52a0eb86f4ba39af73c8359aa2a920132377d630
Fagerlund <henrik.fagerlund1@telia.com>
2016-06-05 23:23:48 +0200

    added javascript and changed tr

---
+++ SmartSchedule/SmartSchedule/JavaScript.js
@@ -0,0 +1,38 @@
+﻿
+function dayFunction() {
+    var d = new Date();
+        day = d.getDay();
+
+        if (day == 0) {
+            document.getElementById("monday").style.background = "rgba(0, 0, 0, 0.5)";
+        }
+        else {
+            document.getElementById("monday").style.background = "transparent";
+        }
+        if (day == 2) {
+            document.getElementById("tuesday").style.background = "rgba(0, 0, 0, 0.5)";
+        }
+        else {
+            document.getElementById("tuesday").style.background = "transparent";
+        }
+        if (day == 4) {
+            document.getElementById("wednesday").style.background = "rgba(0, 0, 0, 0.5)";
+        }
+        else {
+            document.getElementById("wednesday").style.background = "transparent";
+        }
+        if (day == 3) {
+            document.getElementById("thursday").style.background = "rgba(0, 0, 0, 0.5)";
+        }
+        else {
+            document.getElementById("thursday").style.background = "transparent";
+        }
+        if (day == 5) {
+            document.getElementById("friday").style.background = "rgba(0, 0, 0, 0.5)";
+        }
+        else {
+            document.getElementById("friday").style.background = "transparent";
+        }
+}
+
+window.onload = dayFunction;
